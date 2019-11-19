9b934106662945f1353e5ea148e70c3bb42b9ed5
kbbushman <k.bushman@yahoo.com>
2019-11-19 11:05:02 -0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+// ------------------------------------- Routes ------------------------------------- //
+
+app.get('/', (req, res) => {
+  res.send('<h1>AUTH API</h1>');
+});
+
+app.listen(PORT, () => console.log(`Server connected at http://localhost:${PORT}`));
